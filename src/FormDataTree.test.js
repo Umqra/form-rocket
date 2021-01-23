842c95e7c48c40fc0a290bb1d8cf8777c8c6c9a6
Sivukhin Nikita <sivukhin.nikita@yandex.ru>
2021-01-23 19:37:37 +0500

    Cleanup FormDataTree a little bit

---
+++ src/FormDataTree.test.js
@@ -0,0 +1,133 @@
+import { createDataTree } from "./FormDataTree";
+
+test("updateNode", () => {
+    const dataTree = createDataTree();
+    dataTree.updateNode(["root", "a", "0"]);
+    dataTree.updateNode(["root", "b", "1"]);
+    dataTree.updateNode(["root", "a", "1"]);
+    expect(dataTree.children(["root"])).toEqual([["root", "a"], ["root", "b"]]);
+    expect(dataTree.children(["root", "a"])).toEqual([["root", "a", "0"], ["root", "a", "1"]]);
+    expect(dataTree.children(["root", "b"])).toEqual([["root", "b", "1"]]);
+});
+
+test("updateNodeWithData", () => {
+    const dataTree = createDataTree();
+    const leafData = {data: {value: "first", validation: "valid"}, tags: {type: "simple", subtype: "extra-simple"}}
+    const internalData = {data: {context: "user-id"}, tags: {name: "Field-Name"}};
+    dataTree.updateNode(["root", "a", "0"], leafData);
+    expect(dataTree.tryGetNode(["root"])).toEqual({data: {}, tags: {}});
+    expect(dataTree.tryGetNode(["root", "a"])).toEqual({data: {}, tags: {}});
+    expect(dataTree.tryGetNode(["root", "b"])).toBeNull();
+    expect(dataTree.tryGetNode(["root", "a", "0"])).toEqual(leafData);
+    dataTree.updateNode(["root", "a"], internalData);
+    expect(dataTree.tryGetNode(["root", "a"])).toEqual(internalData);
+    dataTree.updateNode(["root", "a", "0"], internalData);
+    expect(dataTree.tryGetNode(["root", "a", "0"])).toEqual({
+        data: {
+            value: "first",
+            validation: "valid",
+            context: "user-id"
+        },
+        tags: {
+            type: "simple",
+            subtype: "extra-simple",
+            name: "Field-Name"
+        }
+    });
+});
+
+test("removeLeafNode", () => {
+    const dataTree = createDataTree();
+    dataTree.updateNode(["root", "a", "0"]);
+    dataTree.updateNode(["root", "b", "1"]);
+    dataTree.updateNode(["root", "a", "1"]);
+    dataTree.removeNode(["root", "a", "0"]);
+    expect(dataTree.children(["root"])).toEqual([["root", "a"], ["root", "b"]]);
+    expect(dataTree.children(["root", "a"])).toEqual([["root", "a", "1"]]);
+    expect(dataTree.children(["root", "b"])).toEqual([["root", "b", "1"]]);
+});
+
+test("removeInternalNode", () => {
+    const dataTree = createDataTree();
+    dataTree.updateNode(["root", "a", "0"]);
+    dataTree.updateNode(["root", "b", "1"]);
+    dataTree.updateNode(["root", "a", "1"]);
+    dataTree.removeNode(["root", "a"]);
+    expect(dataTree.children(["root"])).toEqual([ ["root", "b"]]);
+    expect(dataTree.children(["root", "b"])).toEqual([["root", "b", "1"]]);
+});
+
+test("addThenRemoveNode", () => {
+    const dataTree = createDataTree();
+    dataTree.updateNode(["root", "a", "0"]);
+    dataTree.updateNode(["root", "b", "1"]);
+    dataTree.updateNode(["root", "a", "1"]);
+    dataTree.removeNode(["root", "a", "0"]);
+    dataTree.updateNode(["root", "a", "0"]);
+    expect(dataTree.children(["root"])).toEqual([["root", "a"], ["root", "b"]]);
+    expect(dataTree.children(["root", "a"])).toEqual([["root", "a", "0"], ["root", "a", "1"]]);
+    expect(dataTree.children(["root", "b"])).toEqual([["root", "b", "1"]]);
+});
+
+test("subscribeDataUpdate", () => {
+    let payloadId = 1;
+    const payload = () => {
+        return {data: {value: payloadId++}};
+    }
+    const dataTree = createDataTree();
+    dataTree.updateNode(["root", "1"], payload());
+    dataTree.updateNode(["root", "2"], payload());
+    const subscriptionCalls: any[] = [];
+    dataTree.subscribe(["root", "1"], {
+        update: (node) => subscriptionCalls.push(node.data.value),
+        dependencies: [{kind: "data", value: "value"}]
+    });
+    dataTree.updateNode(["root", "2"], payload());
+    expect(subscriptionCalls).toEqual([]);
+    dataTree.updateNode(["root", "1"], payload());
+    expect(subscriptionCalls).toEqual([4]);
+    dataTree.updateNode(["root", "1"], {data: { validation: "valid" }});
+    expect(subscriptionCalls).toEqual([4]);
+});
+
+test("subscribeChildrenUpdate", () => {
+    const dataTree = createDataTree();
+    dataTree.updateNode(["root", "a", "0"]);
+    const subscriptionCalls: any[] = [];
+    dataTree.subscribe(["root", "a"], {
+        update: (node) => subscriptionCalls.push(dataTree.children(["root", "a"])),
+        dependencies: [{kind: "structure", value: "children"}]
+    });
+    dataTree.updateNode(["root", "a", "0"], {data: {value: "first"}});
+    expect(subscriptionCalls).toEqual([]);
+    dataTree.updateNode(["root", "a", "1"]);
+    expect(subscriptionCalls).toEqual([
+        [["root", "a", "0"], ["root", "a", "1"]]
+    ]);
+    dataTree.removeNode(["root", "a", "0"]);
+    expect(subscriptionCalls).toEqual([
+        [["root", "a", "0"], ["root", "a", "1"]],
+        [["root", "a", "1"]]
+    ]);
+    dataTree.updateNode(["root", "a", "1", "child"]);
+    expect(subscriptionCalls).toEqual([
+        [["root", "a", "0"], ["root", "a", "1"]],
+        [["root", "a", "1"]]
+    ]);
+});
+
+test("unsubscribe", () => {
+    const dataTree = createDataTree();
+    dataTree.updateNode(["root", "1"]);
+    dataTree.updateNode(["root", "2"]);
+    const subscriptionCalls: any[] = [];
+    const unsubscribe = dataTree.subscribe(["root", "1"], {
+        update: (node) => subscriptionCalls.push(node.data.value),
+        dependencies: [{kind: "data", value: "value"}]
+    });
+    dataTree.updateNode(["root", "1"], {data: {value: 1}});
+    expect(subscriptionCalls).toEqual([1]);
+    unsubscribe();
+    dataTree.updateNode(["root", "1"], {data: {value: 2}});
+    expect(subscriptionCalls).toEqual([1]);
+});
--- src/FormDataTree.ts
+++ src/FormDataTree.ts
@@ -7,7 +7,7 @@ export interface FormDataTreeNode {
 
 type FormDataTreeNodeOptional = Partial<FormDataTreeNode>;
 
-type SubscriptionDependency = {data: string} | {tag: string} | {structure: "children"}
+type SubscriptionDependency = {kind: "data", value: string} | {king: "tag", value: string} | {kind: "structure", value: "children"}
 
 interface FormDataTreeSubscription {
     update: (data: FormDataTreeNode) => void;
@@ -18,8 +18,8 @@ export interface FormDataTree {
     updateNode(nodePath: Path, node?: FormDataTreeNodeOptional): void;
     removeNode(nodePath: Path): void;
     tryGetNode(nodePath: Path): FormDataTreeNode | null;
-    children(nodePath: Path): Path[];
     subscribe(nodePath: Path, subscription: FormDataTreeSubscription): () => void;
+    children(nodePath: Path): Path[];
 }
 
 interface TreeNode {
@@ -72,41 +72,60 @@ function removeNode(root: TreeNode, nodePath: Path, l: number, r: number) {
     removeNode(root.children[id], nodePath, l + 1, r);
 }
 
+function matches(dependency: SubscriptionDependency, change: SubscriptionDependency) {
+    if (dependency.kind !== change.kind) {
+        return false;
+    }
+    return dependency.value === change.value;
+}
+
 function triggerSubscriptions(node: TreeNode, changes: SubscriptionDependency[]) {
     // todo (sivukhin, 19.01.2021): Optimize subscription evaluation with some sort of index?
     for (const [_, subscription] of node.subscriptions) {
-        subscription.dependencies[0].
+        let shouldBeTriggered = false;
+        for (const dependency of subscription.dependencies) {
+            if (shouldBeTriggered) {
+                break;
+            }
+            for (const change of changes) {
+                if (matches(dependency, change)) {
+                    shouldBeTriggered = true;
+                    break;
+                }
+            }
+        }
+        if (shouldBeTriggered) {
+            subscription.update(node.dataNode);
+        }
     }
 }
 
-export function createEngine(): FormDataTree {
+export function createDataTree(): FormDataTree {
     const root = createEmptyNode();
     let subscriptionId = 0;
     return {
         updateNode(nodePath: Path, node?: FormDataTreeNodeOptional) {
-            {
-                const previousNode = getNode(root, nodePath, 0, nodePath.length);
-                const currentNode = addNode(root, nodePath, 0, nodePath.length);
+            const previousNode = getNode(root, nodePath, 0, nodePath.length);
+            const currentNode = addNode(root, nodePath, 0, nodePath.length);
 
-                const dependencies: SubscriptionDependency[] = [];
-                if (node != null && node.data != null) {
-                    currentNode.dataNode.data = node.data;
-                    dependencies.push(...Object.keys(node.data).map(x => ({data: x})))
-                }
-                if (node != null && node.tags != null) {
-                    currentNode.dataNode.tags = node.tags;
-                    dependencies.push(...Object.keys(node.tags).map(x => ({tag: x})))
-                }
+            const dependencies: SubscriptionDependency[] = [];
+            if (node != null && node.data != null) {
+                currentNode.dataNode.data = {...currentNode.dataNode.data, ...node.data};
+                dependencies.push(...Object.keys(node.data).map(x => ({kind: "data", value: x})))
+            }
+            if (node != null && node.tags != null) {
+                currentNode.dataNode.tags = {...currentNode.dataNode.tags, ...node.tags};
+                dependencies.push(...Object.keys(node.tags).map(x => ({kind: "tag", value: x})))
+            }
 
-                if (dependencies.length > 0) {
-                    triggerSubscriptions(currentNode, dependencies);
-                }
+            if (dependencies.length > 0) {
+                triggerSubscriptions(currentNode, dependencies);
+            }
 
-                if (previousNode == null) {
-                    const parent = getNode(root, nodePath, 0, nodePath.length - 1);
-                    if (parent != null) {
-                        triggerSubscriptions(parent, [{structure: "children"}]);
-                    }
+            if (previousNode == null) {
+                const parent = getNode(root, nodePath, 0, nodePath.length - 1);
+                if (parent != null) {
+                    triggerSubscriptions(parent, [{kind: "structure", value: "children"}]);
                 }
             }
         },
@@ -115,7 +134,7 @@ export function createEngine(): FormDataTree {
             if (nodePath.length > 0) {
                 const parent = getNode(root, nodePath, 0, nodePath.length - 1);
                 if (parent != null) {
-                    triggerSubscriptions(parent, [{structure: "children"}]);
+                    triggerSubscriptions(parent, [{kind: "structure", value: "children"}]);
                 }
             }
         },
