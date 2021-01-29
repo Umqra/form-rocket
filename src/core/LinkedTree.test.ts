4991a6199b9885bcb9f20de5c2ed58ab8336987f
Sivukhin Nikita <sivukhin.nikita@yandex.ru>
2021-01-29 17:41:17 +0500

    Implement LinkedTree

---
+++ src/core/LinkedTree.test.ts
@@ -0,0 +1,57 @@
+import { createTree } from "./Tree";
+import { createLinkedTree } from "./LinkedTree";
+
+test("singleConnection", () => {
+    const a = createTree();
+    const b = createTree();
+
+    const linkedTree = createLinkedTree({a: a, b: b});
+    linkedTree.connect({a: [["a", "1"]], b: [["b", "1"]]});
+    expect(linkedTree.connections({a: [["a", "1"]]})).toEqual({
+        a: [],
+        b: [["b", "1"]]
+    });
+});
+
+test("manyConnections", () => {
+    const a = createTree();
+    const b = createTree();
+    const c = createTree();
+
+    const linkedTree = createLinkedTree({a: a, b: b, c: c});
+    linkedTree.connect({a: [["a", "1"]], c: [["c", "1"], ["c", "2"]]});
+    linkedTree.connect({b: [["b", "1"]], c: [["c", "2"], ["c", "3"]]});
+    expect(linkedTree.connections({a: [["a", "1"]], b: [["b", "1"]]})).toEqual({
+        a: [],
+        b: [],
+        c: [["c", "2"]]
+    });
+    expect(linkedTree.connections({c: [["c", "1"]]})).toEqual({
+        a: [["a", "1"]],
+        b: [],
+        c: []
+    });
+    expect(linkedTree.connections({c: [["c", "2"]]})).toEqual({
+        a: [["a", "1"]],
+        b: [["b", "1"]],
+        c: []
+    });
+    expect(linkedTree.connections({c: [["c", "3"]]})).toEqual({
+        a: [],
+        b: [["b", "1"]],
+        c: []
+    });
+});
+
+test("noConnections", () => {
+    const a = createTree();
+    const b = createTree();
+    const c = createTree();
+
+    const linkedTree = createLinkedTree({a: a, b: b, c: c});
+    expect(linkedTree.connections({a: [["a", "1"]]})).toEqual({
+        a: [],
+        b: [],
+        c: []
+    });
+})
--- src/core/LinkedTree.ts
+++ src/core/LinkedTree.ts
@@ -15,13 +15,70 @@ type LinkedTree<TLinkedTrees extends LinkedTreesArgs> = {
     connections(nodes: Partial<LinkedTreesPath<TLinkedTrees>>): LinkedTreesPath<TLinkedTrees>;
 }
 
-function createLinkedTree<TLinkedTrees extends LinkedTreesArgs>(trees: TLinkedTrees): LinkedTree<TLinkedTrees> {
+function getKey(treeName: string, path: Path) {
+    return treeName + ":" + path.join(".");
+}
+
+function intersect<T>(a: Set<T>, b: Set<T>): Set<T> {
+    if (a.size > b.size) {
+        return intersect(b, a);
+    }
+    const result = new Set<T>();
+    a.forEach(x => {
+        if (b.has(x)) {
+            result.add(x);
+        }
+    });
+    return result;
+}
+
+export function createLinkedTree<TLinkedTrees extends LinkedTreesArgs>(trees: TLinkedTrees): LinkedTree<TLinkedTrees> {
+    const connections: Map<string, Set<string>> = new Map();
     return {
         ...trees,
-        connect(nodes: LinkedTreesPath<TLinkedTrees>) {},
+        connect(nodes: LinkedTreesPath<TLinkedTrees>) {
+            for (const sourceTree of Object.keys(nodes)) {
+                for (const targetTree of Object.keys(nodes)) {
+                    if (sourceTree === targetTree) {
+                        continue;
+                    }
+                    for (const a of nodes[sourceTree]) {
+                        for (const b of nodes[targetTree]) {
+                            const sourceKey = getKey(sourceTree, a);
+                            const targetKey = getKey(targetTree, b);
+                            if (!connections.has(sourceKey))
+                                connections.set(sourceKey, new Set());
+                            connections.get(sourceKey).add(targetKey);
+                        }
+                    }
+                }
+            }
+        },
         connections(nodes: LinkedTreesPath<TLinkedTrees>): LinkedTreesPath<TLinkedTrees> {
+            let intersection: Set<string> | null = null;
+            for (const sourceTree of Object.keys(nodes)) {
+                for (const a of nodes[sourceTree]) {
+                    const key = getKey(sourceTree, a);
+                    const current = connections.has(key) ? connections.get(key) : new Set<string>();
+                    if (intersection == null) {
+                        intersection = current;
+                    } else {
+                        intersection = intersect(intersection, current);
+                    }
+                }
+            }
             // @ts-ignore
-            return {};
+            const result: LinkedTreesPath<TLinkedTrees> = Object.fromEntries(Object.keys(trees).map(x => [x, []]));
+            intersection.forEach(x => {
+                const [treeName, pathString] = x.split(":");
+                const path = pathString.split(".");
+                if (!result.hasOwnProperty(treeName)) {
+                    // @ts-ignore
+                    result[treeName] = [];
+                }
+                result[treeName].push(path);
+            })
+            return result;
         }
     };
 }
